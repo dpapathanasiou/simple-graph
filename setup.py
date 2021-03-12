10b4007d6322a33fbb5c8200ea68fec47bb00b91
Chris Smith <smitty1e@gmail.com>
2021-03-11 20:42:54 -0500

    add setup.py for pip install -e

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-graph", # Replace with your own username
+    version="0.0.1",
+    author="Denis Papathanasiou",
+    author_email="denis@papathanasiou.org",
+    description='This is a simple graph database in SQLite, inspired by "SQLite as a document database"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dpapathanasiou/simple-graph",
+    project_urls={
+        "Bug Tracker": "https://github.com/dpapathanasiou/simple-graph/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
