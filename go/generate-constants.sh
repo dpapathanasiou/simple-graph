a1f2bdb4a85a4a9c6e71509d1071abba2a9bdeb2
Denis Papathanasiou <denis@papathanasiou.org>
2021-05-16 17:43:02 -0400

    Updated the go package name

--- go/generate-constants.sh
+++ go/generate-constants.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 
-mkdir -p src/constants
-echo 'package constants' > src/constants/constants.go
-echo '\nconst (' >> src/constants/constants.go
+mkdir -p src/simpledb
+echo 'package simpledb' > src/simpledb/constants.go
+echo '\nconst (' >> src/simpledb/constants.go
 
 for file in $(ls ../sql/*.sql)
 do
   sql=$(cat $file)
   val=$(basename $file | sed 's/\.sql//;s/[^-]\+/\L\u&/g;s/-//g')
-  echo "    $val = \`$sql" >> src/constants/constants.go
-  echo '`\n' >> src/constants/constants.go
+  echo "    $val = \`$sql" >> src/simpledb/constants.go
+  echo '`\n' >> src/simpledb/constants.go
 done
 
-echo ')' >> src/constants/constants.go
+echo ')' >> src/simpledb/constants.go
