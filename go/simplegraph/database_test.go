786e53b6da9e1fd42e796edfab16578dc4586c83
Denis Papathanasiou <denis@papathanasiou.org>
2021-05-22 16:15:44 -0400

    More complete test of resolveDbFileReference

--- go/simplegraph/database_test.go
+++ go/simplegraph/database_test.go
@@ -1,14 +1,45 @@
 package simplegraph
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func ErrorMatches(actual error, expected string) bool {
+	if actual == nil {
+		return expected == ""
+	}
+	if expected == "" {
+		return false
+	}
+	return strings.Contains(actual.Error(), expected)
+}
 
 func TestResolveDbFileReference(t *testing.T) {
 	path := "/tmp/database.sqlite?_foreign_keys=true"
-	actualPath, actualErr := resolveDbFileReference("/tmp", "database.sqlite")
+	actualPath, actualPathErr := resolveDbFileReference("/tmp", "database.sqlite")
 	if actualPath != path {
-		t.Errorf("resolveDbFileReference(\"/tmp\", \"database.sqlite\" = %q but expected %q", actualPath, path)
+		t.Errorf("resolveDbFileReference(\"/tmp\", \"database.sqlite\") = %q but expected %q", actualPath, path)
 	}
-	if actualErr != nil {
-		t.Errorf("resolveDbFileReference(\"/tmp\", \"database.sqlite\" = %q but expected nil", actualErr)
+	if actualPathErr != nil {
+		t.Errorf("resolveDbFileReference(\"/tmp\", \"database.sqlite\") = %q but expected nil", actualPathErr)
+	}
+
+	file := "database.sqlite?_foreign_keys=true"
+	actualFile, actualFileErr := resolveDbFileReference("database.sqlite")
+	if actualFile != file {
+		t.Errorf("resolveDbFileReference(\"database.sqlite\") = %q but expected %q", actualFile, file)
+	}
+	if actualFileErr != nil {
+		t.Errorf("resolveDbFileReference(\"database.sqlite\") = %q but expected nil", actualFileErr)
+	}
+
+	empty := "invalid database file reference"
+	emptyFile, emptyFileErr := resolveDbFileReference()
+	if emptyFile != "" {
+		t.Errorf("resolveDbFileReference() = %q but expected %q", emptyFile, "")
+	}
+	if !ErrorMatches(emptyFileErr, empty) {
+		t.Errorf("resolveDbFileReference() = %q but expected %q", emptyFileErr.Error(), empty)
 	}
 }
