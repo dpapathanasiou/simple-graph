5e5093ca33221ea059f7f3a73b2f214d2583eff3
Denis Papathanasiou <denis@papathanasiou.org>
2021-05-25 08:26:56 -0400

    Naming tweak, to reflect the optionality of the target

--- go/simplegraph/database.go
+++ go/simplegraph/database.go
@@ -266,7 +266,7 @@ func FindNodes(properties map[string]string, startsWith bool, contains bool, dat
 	return find(db)
 }
 
-func traverseFromTo(source string, statement string, target string) func(*sql.DB) ([]string, error) {
+func traverseFrom(source string, statement string, target string) func(*sql.DB) ([]string, error) {
 	return func(db *sql.DB) ([]string, error) {
 		stmt, stmtErr := db.Prepare(statement)
 		evaluate(stmtErr)
@@ -305,7 +305,7 @@ func TraverseFromTo(source string, target string, traversal string, database ...
 	db, dbErr := sql.Open(SQLITE, dbReference)
 	evaluate(dbErr)
 	defer db.Close()
-	fn := traverseFromTo(source, traversal, target)
+	fn := traverseFrom(source, traversal, target)
 	return fn(db)
 }
 
@@ -315,6 +315,6 @@ func TraverseFrom(source string, traversal string, database ...string) ([]string
 	db, dbErr := sql.Open(SQLITE, dbReference)
 	evaluate(dbErr)
 	defer db.Close()
-	fn := traverseFromTo(source, traversal, "")
+	fn := traverseFrom(source, traversal, "")
 	return fn(db)
 }
