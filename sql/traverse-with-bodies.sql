be41db28995f745a4f85677bcade7f0471984848
Denis Papathanasiou <denis@papathanasiou.org>
2021-06-14 10:54:55 -0400

    SQL for path traversal capturing node and edge bodies in one go

---
+++ sql/traverse-with-bodies.sql
@@ -0,0 +1,9 @@
+WITH RECURSIVE traverse(x, y, obj) AS (
+  SELECT ?, ?, ?
+  UNION
+  SELECT id, '()', body FROM nodes JOIN traverse ON id = x
+  UNION
+  SELECT source, '<-', properties FROM edges JOIN traverse ON target = x
+  UNION
+  SELECT target, '->', properties FROM edges JOIN traverse ON source = x
+) SELECT x, y, obj FROM traverse;
